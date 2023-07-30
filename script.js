dfb6117316452607b2f9b836ad7b43061b57900b
Catteleya <115270371+Catteleya@users.noreply.github.com>
2023-07-30 12:17:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let darkMode = false;
+
 function showParagraph(paraNumber) {
   const paragraphs = document.querySelectorAll("#paragraph-container p");
   paragraphs.forEach(para => para.classList.add("hidden"));
-  
+
   const selectedParagraph = document.getElementById(`para${paraNumber}`);
   selectedParagraph.classList.remove("hidden");
 }
 
 function toggleTheme() {
+  darkMode = !darkMode;
   const body = document.body;
-  body.classList.toggle("dark-mode");
+  body.classList.toggle("dark-mode", darkMode);
 }
