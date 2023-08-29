18df768b3f706c844b240feb911424f68d5f7b5b
Catteleya <115270371+Catteleya@users.noreply.github.com>
2023-08-29 11:40:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ let cardVisibility = [false, false, false, false, false];
 // Existing code...
 
 function toggleCard(cardNumber) {
+  console.log("Function XYZ executed")
   const selectedCard = document.getElementById(`card${cardNumber}`);
   cardVisibility[cardNumber - 1] = !cardVisibility[cardNumber - 1];
   selectedCard.classList.toggle("hidden", !cardVisibility[cardNumber - 1]);
